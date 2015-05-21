a7d7428e7efabbae86788c0c482ca27bf8ff06d6
Alexandre Kurth <alexandre.kurth@gmail.com>
2015-05-21 18:17:04 +0200

    pep8

--- django_pycnikr/main_app/views.py
+++ django_pycnikr/main_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 stylesheets_dir = join(dirname(__file__), 'stylesheets')
 
 def template(request, name):
-    name=name if name.endswith('.py') else name+'.py'
+    name=name if name.endswith('.py') else name + '.py'
     stylesheet = open(join(stylesheets_dir, name), 'r')
     content = stylesheet.read()
 
