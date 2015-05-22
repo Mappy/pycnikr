2a451eb38d748187309f89773064798760188634
Alexandre Kurth <alexandre.kurth@gmail.com>
2015-05-22 16:39:42 +0200

    Upload of the stylesheet edited in the webpage

--- django_pycnikr/main_app/urls.py
+++ django_pycnikr/main_app/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from main_app.views import template
 
 urlpatterns = [
+    url(r'pycnik/(.*)', 'main_app.views.pycnik'),
     url(r'(.*)', 'main_app.views.template'),
 ]
--- django_pycnikr/main_app/views.py
+++ django_pycnikr/main_app/views.py
@@ -1,18 +1,25 @@
 from os.path import join, dirname
 
 from django.shortcuts import render
+from django.http import HttpResponse
+import pycnik
 
 stylesheets_dir = join(dirname(__file__), 'stylesheets')
 
 def template(request, name):
-    name=name if name.endswith('.py') else name + '.py'
+    name = name if name.endswith('.py') else name + '.py'
     stylesheet = open(join(stylesheets_dir, name), 'r')
     content = stylesheet.read()
 
     return render(
         request, 'main_app/hello.html',
         {
-            "name": "pycnikr",
+            "name": name[:-3],
             'stylesheet_content': content,
         }
     )
+
+def pycnik(request, stylesheet):
+    print stylesheet
+    print request.POST
+    return HttpResponse('Stylesheet successfully applied')
