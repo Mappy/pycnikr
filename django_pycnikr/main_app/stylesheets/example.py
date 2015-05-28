83fbfd290582276b403eeb1ebf72cfe7e9037307
Alexandre Kurth <alexandre.kurth@gmail.com>
2015-05-28 15:23:12 +0200

    refactoring

--- django_pycnikr/main_app/stylesheets/example.py
+++ django_pycnikr/main_app/stylesheets/example.py
@@ -12,8 +12,8 @@ Map.LEVEL_NUMBER = 20
 Map.ZOOM_FACTOR = 2
 
 # Map definition
-#Map.background_color = 'rgb(70,130,180)'  # steelblue
-Map.background_color = 'rgb(154, 205, 50)'  # green
+Map.background_color = 'rgb(70,130,180)'  # steelblue
+#Map.background_color = 'rgb(154, 205, 50)'  # green
 
 Map.srs = '+init=epsg:3857'  # pseudo mercator
 Map.minimum_version = '2.0'
